b602d29b0fe7845eb33295392b8b04d6bfdc8fed
Ridam Jain <138118400+ridamjain-61@users.noreply.github.com>
2024-12-12 01:01:35 +0530

    Create main.py

---
+++ Week1/Assignment/Part2/main.py
@@ -0,0 +1,25 @@
+'''
+    NumPy and The Matrix
+'''
+import numpy as np
+
+def task1(matrix):
+    '''print the upper diagonal matrix in column-wise fashion'''
+    
+
+def task2(matrix):
+    '''print mean, median, std (precision 2), all along x, determinant and inverse/pseudo-inverse'''
+    
+
+def task3(matrix):
+    '''print after sorting along vertical and horizontal and then print flattened array'''
+    
+
+def task4(matrix):
+    '''print the unique frequencies of the sorted array and the frequency of second-largest'''
+    
+
+def task5(matrix, num = 0):
+    '''print the padded matrix'''
+
+# You can test these functions on your own and match with the given output in the out folder
